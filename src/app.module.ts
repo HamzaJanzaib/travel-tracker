86ac79b39217448ffc4dd86284576e0d95ae9b08
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-06 13:41:17 +0500

    feat(throttler): add throttling functionality with ThrottlerModule integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -7,11 +9,27 @@ import { DestinationModule } from './destination/destination.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  }
-  ), AuthModule, DestinationModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60_000,
+        limit: 20,
+      },
+    ]),
+    AuthModule,
+    DestinationModule,
+    HealthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
