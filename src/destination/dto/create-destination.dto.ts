3354ffaf9998b17055a9159c5e96b625f66df740
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-06 12:57:24 +0500

    add CreateDestinationDto with validation rules and remove unused Destination entity

--- src/destination/dto/create-destination.dto.ts
+++ src/destination/dto/create-destination.dto.ts
@@ -1,1 +1,22 @@
-export class CreateDestinationDto {}
+import { IsString, IsNotEmpty, IsOptional, IsDateString, IsInt } from 'class-validator';
+
+export class CreateDestinationDto {
+    @IsString()
+    @IsNotEmpty()
+    name!: string;
+
+    @IsDateString()
+    travelDate!: string;
+
+    @IsString()
+    @IsOptional()
+    notes?: string;
+
+    @IsString()
+    @IsOptional()
+    location?: string;
+
+    @IsInt()
+    @IsOptional()
+    userId?: number;
+}
