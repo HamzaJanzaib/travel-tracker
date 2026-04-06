2a27c52f255b3118f05e86d9d3673f4b3dcf395c
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-06 12:54:48 +0500

    add destination module with controller, service, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from "@nestjs/config";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { DestinationModule } from './destination/destination.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
   }
-  ), AuthModule],
+  ), AuthModule, DestinationModule],
   controllers: [AppController],
   providers: [AppService],
 })
